201eb4fd4095a5492d8c77bd7466bb4401ff40c3
JavierSupr <javiersuprantiyo@gmail.com>
2024-12-22 00:05:41 +0700

    change main.py

--- main.py
+++ main.py
@@ -20,7 +20,9 @@ input_shape = input_details[0]['shape']  # Example: [1, 320, 320, 3]
 def preprocess_frame(frame, input_shape):
     image = cv2.resize(frame, (input_shape[1], input_shape[2]))
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    input_data = np.expand_dims(image, axis=0).astype(np.uint8)  # Use .astype(np.float32) if not quantized
+    # Normalize to [-128, 127] for INT8
+    normalized_image = image.astype(np.float32) - 128
+    input_data = np.expand_dims(normalized_image, axis=0).astype(np.int8)
     return input_data
 
 # Postprocess the model output to print detected labels and confidences
