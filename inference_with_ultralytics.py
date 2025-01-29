804a84932c5375d45387adb9ce39a71255fdda5e
JavierSupr <javiersuprantiyo@gmail.com>
2025-01-29 21:39:51 +0700

    debugging

--- inference_with_ultralytics.py
+++ inference_with_ultralytics.py
@@ -38,7 +38,7 @@ def process_yolo_output(output_data, conf_threshold=0.25):
     predictions = output_data[0]  # Take first batch
     
     for i, detection in enumerate(predictions):
-        print(f"detection{detection}")
+        print(f"{i}detection{detection}")
         confidence = detection[4]  # Object confidence
         if confidence > conf_threshold:
             class_scores = detection[5:]  # Class scores
