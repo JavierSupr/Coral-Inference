3af23d3791e53083745c84450e8c12ed71487f96
JavierSupr <javiersuprantiyo@gmail.com>
2025-01-29 20:38:25 +0700

    debugging

--- metadata.py
+++ metadata.py
@@ -11,5 +11,12 @@ interpreter.allocate_tensors()
 try:
     metadata = interpreter.get_signature_list()
     print("Model Metadata:", metadata)
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+    output_shape = output_details[0]['shape']
+    num_classes = output_shape[-1] - 5  # Subtract 5 (box coordinates)
+    print("Number of Classes:", num_classes)
+    print("Input Details:", input_details)
+    print("Output Details:", output_details)
 except AttributeError:
     print("Metadata not found in this TFLite model.")
