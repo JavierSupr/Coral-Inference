2c29f131be11b1efdc129bf15924bf3979ea75e8
JavierSupr <javiersuprantiyo@gmail.com>
2025-02-04 21:14:28 +0700

    add segmentation + detection program

---
+++ edgetpusilva.py
@@ -0,0 +1,7 @@
+from edge_tpu_silva import process_detection
+
+# Run the object detection process
+outs = process_detection(model_path='240_yolov8n_full_integer_quant_edgetpu.tflite', input_path='333-vid-20231011-170120_Tt2GmTrq.mp4', imgsz=192)
+
+for _, _ in outs:
+  pass
