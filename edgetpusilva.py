39c3fafd03ebd0fbc81080160f6a5ee6a0ffc20e
JavierSupr <javiersuprantiyo@gmail.com>
2025-02-04 22:49:01 +0700

    add segmentation + detection program

--- edgetpusilva.py
+++ edgetpusilva.py
@@ -48,11 +48,6 @@ def process_segmentation(
     start_time = time.time()
     for out in outs:
         masks = out.masks
-        frame = out.plot()  # Draw the segmentation on the frame
-        _, buffer = cv2.imencode('.jpg', frame)
-        frame_bytes = buffer.tobytes()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
 
         if verbose:
             print("\n\n-------RESULTS--------")
