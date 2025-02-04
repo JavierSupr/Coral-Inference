c721cdc7b412755858e98aca9e4cafa5661fd4ba
JavierSupr <javiersuprantiyo@gmail.com>
2025-02-04 21:56:07 +0700

    add segmentation + detection program

--- edgetpusilva.py
+++ edgetpusilva.py
@@ -4,18 +4,34 @@ from typing import Tuple, Union, List
 import cv2
 from ultralytics import YOLO
 
-
-def process_detection(
+def process_segmentation(
     model_path: str,
     input_path: str,
     imgsz: Union[int, Tuple[int, int]],
-    threshold: float = 0.4,
+    threshold: int = 0.4,
     verbose: bool = True,
     show: bool = False,
     classes: List[int] = None,
 ):
+    """Run object segmentation with with edge-tpu-silva
+
+    Args:
+        model_path (str): Define a .tflite model
+        input_path (str): File path of image/video to process | Camera(0|1|2).
+        imgsz (Union[int, Tuple[int, int]]): Defines the image size for inference. Can be a single integer 640 for square resizing or a (height, width) tuple. This should be same as what was used to export the YOLO model.
+        threshold (int, optional): Threshold for detected objects. Defaults to 0.4.
+        verbose (bool, optional): Display prints to terminal. Defaults to True.
+        show (bool, optional): Display frame with detection. Defaults to False.
+        classes (List[int], optional): Filters predictions to a set of class IDs. Only detections belonging to the specified classes will be returned. Defaults to None.
+
+    Yields:
+        (Gen): frame, objs_lst, fps
+    """
+
     # Load a model
-    model = YOLO(model=model_path, task="segment")
+    model = YOLO(
+        model=model_path, task="segment", verbose=False
+    )  # Load a official model or custom model
 
     # Run Prediction
     outs = model.predict(
@@ -29,8 +45,7 @@ def process_detection(
     )
 
     frame_count = 0
-    prev_time = time.time()
-    
+    start_time = time.time()
     for out in outs:
         masks = out.masks
         if verbose:
@@ -53,11 +68,12 @@ def process_detection(
                 print("  score: ", conf)
                 print("  seg:  ", type(seg))
 
-
         frame_count += 1
+        elapsed_time = time.time() - start_time
+        fps = frame_count / elapsed_time
 
         if verbose:
-            print("----INFERENCE TIME----")
+            print("\n----INFERENCE TIME----")
             print("FPS: {:.2f}".format(fps))
 
         yield objs_lst, fps
@@ -65,7 +81,6 @@ def process_detection(
         # Break the loop if 'esc' key is pressed for video or camera
         if cv2.waitKey(1) == 27:
             break
-
 # Define paths and parameters
 model_path = "240_yolov8n-seg_full_integer_quant_edgetpu.tflite"
 input_path = "333-vid-20231011-170120_Tt2GmTrq.mp4"
@@ -76,7 +91,7 @@ show = False
 classes = None
 
 # Call the function and process the output
-for objects, fps in process_detection(
+for objects, fps in process_segmentation(
     model_path, input_path, imgsz, threshold, verbose, show, classes
 ):
     print("\nDetected Objects:", objects)
