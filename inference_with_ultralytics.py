a4316428946047385138c3a6434580e4adb77c3c
JavierSupr <javiersuprantiyo@gmail.com>
2025-02-22 09:02:05 +0700

    debugging

--- inference_with_ultralytics.py
+++ inference_with_ultralytics.py
@@ -34,7 +34,7 @@ def generate_frames():
         prev_time = curr_time
 
         # Run YOLOv8 inference
-        results = model.predict(frame, conf=CONF_THRESHOLD, iou = 0.5, imgz = IMGSZ, verbose=False)
+        results = model.predict(frame, conf=CONF_THRESHOLD, iou = 0.5, imgsz = IMGSZ, verbose=False)
         for out in results:
             masks = out.masks
             for index, box in enumerate(out.boxes):
