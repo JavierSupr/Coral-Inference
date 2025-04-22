6eef52bf33eeacbd6346b1f0dd97c18528994dbf
JavierSupr <javiersuprantiyo@gmail.com>
2025-04-22 10:39:05 +0700

    change input video to stream

--- stream_inference.py
+++ stream_inference.py
@@ -17,28 +17,33 @@ sock.bind(("0.0.0.0", PORT_1))
 sock.settimeout(1.0)
 
 def receive_udp_stream():
-    while True:
-        try:
-            data, _ = sock.recvfrom(4)
-            print(len(data))
-            frame_id = struct.unpack("I", data)[0]
-            num_chunks, _ = sock.recvfrom(1)
-            num_chunks = struct.unpack("B", num_chunks)[0]
+    try:
+        # Abaikan frame_id
+        sock.recvfrom(4)
+        
+        # Terima jumlah potongan
+        num_chunks_data, _ = sock.recvfrom(1)
+        num_chunks = struct.unpack("B", num_chunks_data)[0]
 
-            chunks = []
-            for _ in range(num_chunks):
-                chunk, _ = sock.recvfrom(BUFFER_SIZE)
-                chunks.append(chunk)
+        # Terima semua potongan
+        chunks = []
+        for _ in range(num_chunks):
+            chunk, _ = sock.recvfrom(BUFFER_SIZE)
+            chunks.append(chunk)
 
-            buffer = b"".join(chunks)
-            npdata = np.frombuffer(buffer, dtype=np.uint8)
-            frame = cv2.imdecode(npdata, cv2.IMREAD_COLOR)
-            return frame, frame_id
-        except socket.timeout:
-            return None, None
-        except Exception as e:
-            print(f"[ERROR] Receiving UDP stream: {e}")
-            return None, None
+        # Gabungkan potongan dan decode
+        buffer = b"".join(chunks)
+        npdata = np.frombuffer(buffer, dtype=np.uint8)
+        frame = cv2.imdecode(npdata, cv2.IMREAD_COLOR)
+        return frame
+    except socket.timeout:
+        print(f"[ERROR] timeout")
+
+        return None
+    except Exception as e:
+        print(f"[ERROR] Receiving UDP stream: {e}")
+        return None
+    
 
 def process_stream(model_path):
     model = YOLO(model_path, task="segment")
@@ -66,12 +71,12 @@ def process_stream(model_path):
                     label = out.names[int(box.cls.numpy()[0])]
                     class_count[label] = class_count.get(label, 0) + 1
 
-            summary = ", ".join([f"{v} {k}" for k, v in class_count.items()])
+            #summary = ", ".join([f"{v} {k}" for k, v in class_count.items()])
             fps = 1 / (time.time() - prev_time)
             prev_time = time.time()
 
-            print(f"[INFO] Frame ID: {fid} - FPS: {fps:.2f} - {summary}")
-            writer.writerow([fid, f"{fps:.2f}", summary])
+            print(f"[INFO] Frame ID: - FPS: {fps:.2f}")
+            #writer.writerow([fid, f"{fps:.2f}", summary])
             time.sleep(max(0, frame_delay - (time.time() - start_time)))
 
 if __name__ == "__main__":
