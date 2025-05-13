4d117ecbd35bce09ec4320218bed89fadc945c83
JavierSupr <javiersuprantiyo@gmail.com>
2025-05-13 12:20:17 +0700

    add multi threading

--- stream_inference.py
+++ stream_inference.py
@@ -74,7 +74,7 @@ def process_stream(model_path, video_port):
                 continue
 
             frame_copy = cv2.resize(frame,(256, 256))
-            results = model.predict(frame_copy, conf=0.65, iou=0.2, imgsz=256, verbose=True)
+            results = model.predict(frame_copy, conf=0.3, iou=0.2, imgsz=256, verbose=True)
             print("4")
             class_count = {}
             for out in results:
